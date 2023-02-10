c6b230260f4cfe9902a64e146fffae328fdb0b47
Arno <3251136656@qq.com>
2023-02-10 17:53:23 +0800

    first commit

---
+++ src/utils/request.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+// 创建axios实例
+const service = axios.create({
+    baseURL: 'nnsicn',
+    timeout: 10000,
+    // headers: {'X-Custom-Header': 'foobar'}
+  });
+
+//   请求拦截器
+  axios.interceptors.request.use(config=>{
+    console.log(config);
+    return config
+  },error=>{
+    console.log(error);
+    return Promise.reject(error);
+  })
+
+//   响应拦截器
+axios.interceptors.response.use(res=>{
+    console.log(res);
+    return res;
+},error=>{
+    console.log(error);
+    return Promise.reject(error);
+})
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+/**
+ * detail
+ * https://cli.vuejs.org/config/#publicpath
+ * https://cli.vuejs.org/zh/config/#vue-config-js
+ */
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/',//根路径
+  lintOnSave: process.env.NODE_ENV === 'development',// https://blog.csdn.net/qq_30351747/article/details/124668658
+  outputDir: 'dist',//当运行 vue-cli-service build 时生成的生产环境构建文件的目录。注意目标目录的内容在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。
+  assetsDir: 'static',//放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  devServer:{//https://webpack.docschina.org/configuration/dev-server/#devserverport
+    port:"1010",//端口号
+    open: true,//告诉 dev-server 在服务器已经启动后打开浏览器。设置其为 true 以打开你的默认浏览器。
+  },
 })
