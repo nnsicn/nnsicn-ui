f29ee23f84cd3fffbe155bc1af682dea6e8467ea
32251136656@qq.com <3251136656@qq.com>
2023-03-02 20:49:43 +0800

    codemirror

---
+++ src/components/sqlEditor/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div ref="editor" class="code-editor"></div>
+</template>
+
+<script>
+import CodeMirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/3024-day.css";
+import "codemirror/theme/monokai.css";
+import "codemirror/mode/sql/sql";
+import "codemirror/mode/javascript/javascript.js";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/hint/show-hint.js";
+import "codemirror/addon/hint/sql-hint.js";
+import "codemirror/addon/hint/javascript-hint.js";
+export default {
+  name: "codeEditor",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      codeMirror: null,
+    };
+  },
+  watch:{
+    "options.mode":{
+      handler(n){
+        if(this.codeMirror){
+          this.codeMirror.setOption("mode",n)
+        }
+      }
+    }
+  },
+  mounted() {
+    this.codeMirror = new CodeMirror(this.$refs.editor, {
+      ...this.options,
+      value: this.value,
+    });
+    console.log(this.codeMirror);
+    this.codeMirror.on("change", (cm) => {
+      this.$emit("input", this.codeMirror.getValue());
+      if (
+        cm.getValue().at(-1) != ";" &&
+        cm.getValue().at(-1) != " " &&
+        cm.getValue().length > 0
+      ) {
+        cm.showHint({
+          completeSingle: false,
+        });
+      }
+      // console.log(cm.getCursor());//获取光标的方法
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+.code-editor {
+  width: 100%;
+  height: 100%;
+  .CodeMirror {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/views/CodeMirror/index.vue
+++ src/views/CodeMirror/index.vue
@@ -1,19 +1,65 @@
 <template>
-  <div>
-    <code-sql v-model="code"></code-sql>
+  <div class="code-mirror">
+    <div class="select-language">
+      <el-select
+        v-model="language"
+        placeholder="请选择语言"
+        :disabled="!!code"
+        @change="selectLanguage"
+      >
+        <el-option label="javaScript" value="text/javascript"> </el-option>
+        <el-option label="SQL" value="text/x-sql"> </el-option>
+      </el-select>
+    </div>
+    <div class="editor">
+      <code-sql v-model="code" :options="options"></code-sql>
+    </div>
   </div>
 </template>
 
 <script>
-import  codeSQL  from "@/views/component/sqlEditor/index.vue";
+import codeSQL from "@/components/sqlEditor/index.vue";
 export default {
-  name:"codemerror",
-  components: { "code-sql":codeSQL },
-  data(){
-    return {code:"qwe"}
-  }
+  name: "codemerror",
+  components: { "code-sql": codeSQL },
+  data() {
+    return {
+      language: "text/javascript",
+      code: "",
+      options: {
+        mode: "text/javascript",
+        lineNumbers: true,
+        theme: "monokai",
+      },
+    };
+  },
+  methods: {
+    selectLanguage(val) {
+      this.options.mode = val
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+.code-mirror {
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  background-color: #eee;
+  .select-language {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 40px;
+    width: 100%;
+    height: 40px;
+    .el-select {
+      width: 600px;
+    }
+  }
+  .editor {
+    // width: 900px;
+    height: 600px;
+  }
+}
 </style>
